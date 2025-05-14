68931bc4164b0519cec63c9282e0a80123d96d9a
ayafounes <founesaya19@gmail.com>
2025-05-14 13:36:23 +0100

    back_Final

--- src/main/java/com/example/produits/security/SecParams.java
+++ src/main/java/com/example/produits/security/SecParams.java
@@ -3,7 +3,7 @@ package com.example.produits.security;
 public interface SecParams {
 
 	public static final long EXP_TIME = 10*24*60*60;
-	public static final String SECRET = "rahmabh@gmail.com";
+	public static final String SECRET = "aya@gmail.com";
 	public static final String PREFIX = "Bearer ";
 
 
